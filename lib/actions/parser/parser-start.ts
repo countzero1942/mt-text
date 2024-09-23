fa33d40c48c6758c4af6c876d18180a735bfd3b2
countzero1942 <countzero1942@yahoo.com>
2024-09-23 19:44:10 -0400

    LogSplitHeads GetPreLineInfo

--- lib/actions/parser/parser-start.ts
+++ lib/actions/parser/parser-start.ts
@@ -8,6 +8,13 @@ import { open } from "node:fs/promises";
 
 const getTextFile = (name: string) => `./text/parser/${name}`;
 
+type PreLineInfo = {
+	readonly type: "PreLineInfo";
+	readonly line: string;
+	readonly indent: number;
+	readonly lineNumber: number;
+};
+
 type LineInfo = {
 	readonly lineInfo: {
 		readonly line: string;
@@ -37,6 +44,64 @@ type ParseErr = Simplify<
 		readonly message: string;
 	} & LineInfo
 >;
+
+type TabsAndContent = {
+	tabs: number;
+	content: string;
+};
+
+const getTabsAndContent = (line: string): TabsAndContent => {
+	const tabRegEx = /^(?<tabs>\t+)?(?<content>.*)/g;
+	type TabContentGroups =
+		| {
+				tabs?: string;
+				content?: string;
+		  }
+		| undefined;
+	const res = tabRegEx.exec(line);
+
+	const groups: TabContentGroups = res?.groups;
+
+	return {
+		tabs: groups?.tabs?.length ?? 0,
+		content: groups?.content ?? "",
+	};
+};
+
+const getPreLineInfo = (
+	line: string,
+	lineNumber: number
+): PreLineInfo | ParseErr => {
+	const getSpaceError = (): ParseErr => {
+		return {
+			type: "ParseErr",
+			message: "Spaces cannot be used to indent lines, only tabs.",
+			lineInfo: {
+				indent: 0,
+				line,
+				lineNumber,
+			},
+		};
+	};
+
+	if (line.startsWith(" ")) {
+		return getSpaceError();
+	}
+
+	const { tabs, content } = getTabsAndContent(line.trimEnd());
+
+	if (content.startsWith(" ")) {
+		return getSpaceError();
+	}
+
+	const preLineInfo: PreLineInfo = {
+		type: "PreLineInfo",
+		line: content,
+		indent: tabs,
+		lineNumber,
+	};
+	return preLineInfo;
+};
 
 const setLineColumn = (
 	lineInfo: LineInfo,
@@ -92,154 +157,86 @@ const splitHead = (
 	};
 };
 
-type ExtractGroupNames<S extends string> =
-	S extends `${string}(?<${infer Name}>${infer Rest}`
-		? Record<Name, string> & ExtractGroupNames<Rest>
-		: Record<never, any>;
+export const logPushArray = () => {
+	const things: string[] = [];
 
-interface MyMatch {
-	tabs?: string;
-	content?: string;
-}
-
-type MyType = {
-	tabs?: string;
-	content?: string;
-};
-
-const getLineIndent = (line: string) => {
-	const tabRegEx = /^(?<tabs>\t+)?(?<content>.*)/g;
-	type TabContentGroups =
-		| {
-				tabs?: string;
-				content?: string;
-		  }
-		| undefined;
-
-	const res = tabRegEx.exec(line);
-
-	log(`Match line: "${line}"`);
-	log(`Regex string: "${tabRegEx.source}"`);
-	log(res);
-	logln(20);
-	const groups1: MyMatch | undefined = res?.groups;
-	const groups2: TabContentGroups = res?.groups;
-	log("groups1");
-	log(groups1);
-	logln(20);
-	log("groups2");
-	log(
-		`tab count: ${groups2?.tabs?.length ?? 0}, ` +
-			`content: "${groups2?.content}"`
-	);
-	logln(40);
-};
-
-type TabsContent = {
-	tabs: number;
-	content: string;
-};
-
-const getTabsContent1 = (line: string): TabsContent => {
-	const tabRegEx = /^(?<tabs>\t+)?(?<content>.*)/g;
-	type TabContentGroups =
-		| {
-				tabs?: string;
-				content?: string;
-		  }
-		| undefined;
-	const res = tabRegEx.exec(line);
-
-	const groups: TabContentGroups = res?.groups;
-
-	log("res1:");
-	log(res);
-
-	return {
-		tabs: groups?.tabs?.length ?? 0,
-		content: groups?.content ?? "",
-	};
-};
-
-const getTabsContent2 = (line: string): TabsContent => {
-	const tabRegEx = /^(\t+)?(.*)/g;
-	const res = tabRegEx.exec(line);
-	if (res === null) {
-		throw Error("Never");
+	for (let i = 0; i < 10; i++) {
+		log(`array length: ${things.length}`);
+		log(`push: ${i}`);
+		things.push(i.toString());
 	}
-	const tabs = res[1]?.length ?? 0;
-	const content = res[2] ?? "";
-
-	log("res2:");
-	log(res);
-
-	return {
-		tabs,
-		content,
-	};
-};
-
-const scrubLine = (line: string) => {
-	return line.replaceAll("\t", "\\t");
-};
-
-const testExtractTabsContent = (line: string) => {
-	log(`line: "${scrubLine(line)}"`);
-	logln(20);
-	const res1 = getTabsContent1(line);
-	log("getTabsContent1:");
-	log(res1);
-	logln(20);
-	const res2 = getTabsContent2(line);
-	log("getTabsContent2:");
-	log(res2);
+	log(`array length: ${things.length}`);
 	logln(40);
+	log(things);
 };
-
-const testLines = () => {
-	logh("Test Extract Tabs and Content");
-
-	testExtractTabsContent("");
-	testExtractTabsContent("\t");
-	testExtractTabsContent("Content");
-	testExtractTabsContent("\tContent");
-	testExtractTabsContent("\t\tContent");
-};
-
-const testGetLineIndent = () => {
-	logh("Test Extract Tabs and Content");
-	getLineIndent("");
-	getLineIndent("\t");
-	getLineIndent("Content");
-	getLineIndent("\tContent");
-	getLineIndent("\t\tContent");
-};
-
-const getLineInfo = (line: string, lineNumber: number) => {};
 
 export const logSplitHeads = async () => {
-	const file = await open(getTextFile("00-start.txt"));
+	logh("Log Parse Begin 01");
 
-	// testGetLineIndent();
-	testLines();
-	let i = 0;
+	const file = await open(getTextFile("00-with-errs.txt"));
+
+	const errors: ParseErr[] = [];
+	const dict = new Map<string, any>();
+
+	const logLineInfo = (li: LineInfo) => {
+		const { line, lineNumber, indent } = li.lineInfo;
+		log(
+			`   (${lineNumber}), indent: ${indent}, ` + `line: "${line}"`
+		);
+	};
+	const logError = (err: ParseErr) => {
+		log(`ParseErr: (${errors.length}): "${err.message}"`);
+		logLineInfo(err as LineInfo);
+		logln(40);
+	};
+
+	let lineNumber = 1;
 	for await (const line of file.readLines()) {
-		// const res = splitHead(line, i);
-		// if (isError(res)) {
-		// 	const { line, lineNumber, message } = res;
-		// 	logh("Error");
-		// 	log(`Error message: "${message}"`);
-		// 	log(`Line: "${line}"`);
-		// 	log(`Line number: ${lineNumber}`);
-		// } else {
-		// 	const { key, value } = res;
-		// 	logh("Key-value");
-		// 	log(`Key: ${key}`);
-		// 	log(`Value: ${value}`);
-		// }
-		// logln(40);
+		const res1 = getPreLineInfo(line, lineNumber);
+		if (res1.type === "ParseErr") {
+			const err = res1;
+			errors.push(res1);
+			logError(err);
+			continue;
+		}
+		const lineInfo: LineInfo = {
+			lineInfo: {
+				line: res1.line,
+				lineNumber: res1.lineNumber,
+				indent: res1.indent,
+			},
+		};
 
-		i++;
+		const res2 = splitHead(lineInfo);
+		switch (res2.type) {
+			case "KeyValueHead":
+				{
+					const { keyHead, valueHead } = res2;
+					log(
+						`KeyValueHead: keyHead: "${keyHead}", ` +
+							`valueHead: "${valueHead}"`
+					);
+					logLineInfo(lineInfo);
+					logln(40);
+				}
+				break;
+			case "EmptyLine":
+				{
+					log("EmptyLine");
+					logLineInfo(lineInfo);
+					logln(40);
+				}
+				break;
+			case "ParseErr":
+				{
+					const err = res2;
+					errors.push(err);
+					logError(err);
+				}
+				break;
+		}
+
+		lineNumber++;
 	}
 };
 
--- lib/ui/parser/ParserStart.tsx
+++ lib/ui/parser/ParserStart.tsx
@@ -1,5 +1,6 @@
 import {
 	logParserLines,
+	logPushArray,
 	logSplitHeads,
 } from "@/actions/parser/parser-start";
 import { log, logh, logln } from "@/utils/log";
@@ -15,6 +16,7 @@ import { testGetKeysAndValuesWithFuncs } from "@/actions/type-fun/one-of-interfa
 
 export default async function ParserStart() {
 	//	await logParserLines();
+	// logPushArray();
 	await logSplitHeads();
 	//	await new Promise((resolve) => setTimeout(resolve, 100));
 	// testGetKeysAndValuesWithFuncs();
